2cb6dde17d42131791da9ba822ad21267cb35e4b
Marlonilao <176732593+Marlonilao@users.noreply.github.com>
2026-09-15 10:06:33 +0800

    make logout a POST request

--- src/routes/logoutRouter.js
+++ src/routes/logoutRouter.js
@@ -2,6 +2,6 @@ const { Router } = require('express');
 const router = Router();
 const controller = require('../controllers/logoutController');
 
-router.get('/', controller.logout);
+router.post('/', controller.logout);
 
 module.exports = router;
